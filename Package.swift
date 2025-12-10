65fc174a4bb700af8738c70e8f8b0427c0443ff6
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-10 08:55:59 +0100

    Update dependency versions to latest tags

--- Package.swift
+++ Package.swift
@@ -18,9 +18,9 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/swift-standards/swift-standards", from: "0.10.0"),
-        .package(url: "https://github.com/swift-standards/swift-incits-4-1986", from: "0.6.2"),
-        .package(url: "https://github.com/swift-standards/swift-ipv4-standard", from: "0.1.2"),
-        .package(url: "https://github.com/swift-standards/swift-ipv6-standard", from: "0.1.2"),
+        .package(url: "https://github.com/swift-standards/swift-incits-4-1986", from: "0.6.3"),
+        .package(url: "https://github.com/swift-standards/swift-ipv4-standard", from: "0.1.3"),
+        .package(url: "https://github.com/swift-standards/swift-ipv6-standard", from: "0.1.3"),
     ],
     targets: [
         .target(
